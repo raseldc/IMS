72e2bd43456cda4dc86285ceff56cdf4bd05f152
Abu Taleb <abutaleb142@gmail.com>
2023-03-05 19:47:30 +0600

    Initial Design Update

---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import { Card, CardHeader, CardBody, CardTitle, CardText, CardLink } from 'reactstrap'
+
+const Home = () => {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome to IMS 🚀</CardTitle>
+        </CardHeader>
+        <CardBody>
+          <CardText>All the best for your new project.</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
+
+export default Home
